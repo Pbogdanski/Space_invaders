ae7c03e364b32bf95b1f9066df6a3d3fb7a617bf
Hubert07 <gajewski.hubert95@gmail.com>
2018-03-24 19:58:54 +0100

    dodanie pkt i var

--- js/app_jq.js
+++ js/app_jq.js
@@ -1,4 +1,26 @@
 // kod projektu [wpisz nazwę]
 $(document).ready(function(){
-    ;
+    var canvas = $('#canvas');
+    var ptak = $('#ptak');
+    var pole = $('.pole');
+    var pole1 = $('#pole1');
+    var pole2 = $('#pole2');
+    var punkty = $('#punkty');
+    var speed = $('#speed');
+    var restart_btn =$('#restart_btn')
+
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var pole_initial_position = parseInt(pole.css('right'));
+    var pole_initial_height = parseInt(pole.css('height'));
+    var bird_left = parseInt(bird.css('left'));
+    var bird_height = parseInt(bird.height());
+    var speed = 10;
+    var go_up = false;
+    var game_over = false;
+
+    var gra = setInterval(function () {
+      if (colision(ptak, pole))
+    } )
+
 });
